ac1a314ea986d8e9bfffe4d04c143f80608f7ec7
devavratwadekar <devavrat.wadekar@gmail.com>
2026-02-08 19:38:32 +0530

    add Data Type notes and code

---
+++ DataType.java
@@ -0,0 +1,20 @@
+public class DataType {
+    public static void main(String[] args) {
+        int i = 10;
+
+        byte b = 127;
+
+        short s = 13365;
+
+        long l = 3456789l;
+
+        float f = 456.45f;
+
+        double d = 4567.4567;
+
+        char c = 'D';
+
+        boolean bool = true;
+        
+    }
+}
