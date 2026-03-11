e3ff69a377a8eb5b89f21e6a01da40a08556c10f
devavratwadekar <devavrat.wadekar@gmail.com>
2026-03-11 17:04:04 +0530

    commit - Jagged Array Example

---
+++ Jagged_Array_Exacmple.java
@@ -0,0 +1,31 @@
+
+public class Jagged_Array_Exacmple {
+
+    public static void main(String[] args) {
+        int nums[][] = new int[3][3];
+
+        nums[0] = new int[3];
+        nums[1] = new int[6];
+        nums[2] = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                nums[i][j] = (int) (Math.random() * 100);
+            }
+        }
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = 0; j < nums[i].length; j++) {
+        //         System.out.print(nums[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        for (int n[] : nums) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
